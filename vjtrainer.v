9909f5579d053a3adb0e719c3fa9b080f3efb9c5
Markus-Amano <markus.a.amano@gmail.com>
2022-08-21 15:42:26 +0800

    base white background

--- vjtrainer.v
+++ vjtrainer.v
@@ -1,5 +1,27 @@
 module main
 
+import gg
+import gx
+
+const (
+    awidth  = 600
+    aheight = 400
+    atitle  = "V Japanese Trainer"
+)
+
 fn main() {
-	println('Hello World!')
+    mut context := gg.new_context(
+        bg_color: gx.white
+        width: awidth
+        height: aheight
+        window_title: atitle
+        frame_fn: frame
+        resizable: false
+    )
+    context.run()
+}
+
+fn frame(mut ctx gg.Context) {
+    ctx.begin()
+    ctx.end()
 }
