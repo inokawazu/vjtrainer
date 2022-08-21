292fb4f4d989efd1cac7173cb7a4db8f1ccd08b6
Markus-Amano <markus.a.amano@gmail.com>
2022-08-21 19:03:59 +0800

    resizing works now

--- vjtrainer.v
+++ vjtrainer.v
@@ -25,14 +25,17 @@ fn main() {
         frame_fn: frame
         resizable: false
         font_path: afpath
+        event_fn: on_event
     )
     context.run()
 }
 
 fn frame(mut ctx gg.Context) {
     ctx.begin()
+    ws := gg.window_size()
+    ww, wh := f32(ws.width), f32(ws.height)
     // ctx.draw_text(0, 0, "今日は This is some text!", color: gx.dark_green, bold: true, size: 32)
-		draw_text_box(mut ctx, awidth/10, aheight/10, 8*awidth/10, 4*aheight/10, "今日は This is some text! サヨナラ")
+		draw_text_box(mut ctx, ww/10, wh/10, 8*ww/10, 4*wh/10, "今日は This is some text! サヨナラ")
     // ctx.draw_rect_filled(8/10*awidth, 1/10*aheight, 8/10*awidth, 4/10*aheight, gx.green)
     // ctx.draw_rect_filled(100, 100, 100, 100, gx.green)
     ctx.end()
@@ -42,4 +45,13 @@ fn draw_text_box(mut ctx gg.Context, x f32, y f32, w f32, h f32, text string) {
 		// font_size := 16
     ctx.draw_rect_filled(x, y, w, h, abox_color)
     ctx.draw_text(int(x), int(y), text, color: atxt_color, size: font_size)
+}
+
+fn on_event(e &gg.Event, mut ctx gg.Context) {
+    match e.typ {
+        .resized {
+            // println(gg.window_size())
+        }
+        else {}
+    }
 }
