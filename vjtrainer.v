f78519daa10bc0025b9a3fe05fc564f1cc127490
Markus-Amano <markus.a.amano@gmail.com>
2022-08-28 13:51:17 +0800

    made the textboxes hidable

--- vjtrainer.v
+++ vjtrainer.v
@@ -15,6 +15,7 @@ const (
     abox_color = gx.light_gray
     atxt_color = gx.black
 		font_size  = 16
+    empty_string = "Hidden"
 )
 
 [heap]
@@ -22,7 +23,9 @@ struct App {
 mut:
     window &ui.Window = unsafe { 0 }
     jp_text string
+    en_text_cache string
     en_text string
+    tb_width int = 9*awidth/10
 }
 
 
@@ -36,50 +39,83 @@ fn main() {
     app.window = ui.window(
         width: aheight, height: aheight,
         title: atitle,
+        on_init: app.on_init,
     )
     
     app.window.children << ui.column(
-        widths: f64(9*awidth/10),
+        widths: f64(app.tb_width),
         alignment: .center,
         spacing: 10
         children: [
-            ui.textbox(
-                id: "jp_text"
-                height: aheight/8
-                is_wordwrap: true
-                is_multiline: true
-                text: &app.jp_text
-                is_sync: true
-                read_only: true
-                fitted_height: false
+        ui.textbox(
+            id: "jp_text"
+            height: aheight/8
+            is_wordwrap: true
+            is_multiline: true
+            text: &app.jp_text
+            is_sync: true
+            read_only: true
+            fitted_height: false
+        ),
+        ui.textbox(
+            id: "en_text"
+            height: aheight/8
+            is_wordwrap: true
+            is_multiline: true
+            text: &app.en_text
+            is_sync: true
+            read_only: true
+            fitted_height: false
+        ),
+        ui.row(
+            spacing: 5
+            children: [
+            ui.button(
+                id: "en_reveal_button"
+                text: "英訳現す"
+                on_click: app.en_reveal
+                text_size: 1.0 / 20
+                radius: .25
+                hoverable: true
             ),
-            ui.textbox(
-                id: "en_text"
-                height: aheight/8
-                is_wordwrap: true
-                is_multiline: true
-                text: &app.en_text
-                is_sync: true
-                read_only: true
-                fitted_height: false
+            ui.button(
+                id: "next_button"
+                text: "次ぎ"
+                on_click: app.next_entry
+                text_size: 1.0 / 20
+                radius: .25
+                hoverable: true
             ),
-            ui.row(
-                children: [
-                ui.button(
-                    text: "Next"
-                    // on_click: app.btn_click
-                    text_size: 1.0 / 20
-                    radius: .25
-                    hoverable: true
-                )
-                ]
-            )
+            ]
+        )
         ]
     )
-
+    
     ui.run(app.window)
 
     println("Finished Running!")
+}
+
+fn (mut app App) on_init(window &ui.Window) {
+    app.en_hide()
+}
+
+fn (app &App) en_reveal (btn &ui.Button) {
+    println("Revealed the English Text.")
+    mut en_tb := app.window.textbox("en_text")
+    en_tb.set_text(app.en_text_cache)
+}
+
+fn (mut app App) en_hide () {
+    println("Hid the English Text.")
+    mut en_tb := app.window.textbox("en_text")
+
+    app.en_text_cache = app.en_text
+    en_tb.set_text(empty_string)
+}
+
+fn (mut app App) next_entry(nt &ui.Button) {
+    app.en_hide()
 }
 
 // fn main() {
